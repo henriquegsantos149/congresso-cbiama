7c7fdb2eed673798b89a802bca875cfb3b975090
Henrique Gonzalez <henriquegonzalez@poli.ufrj.br>
2026-07-06 10:20:54 -0300

    add vite config to build multiple pages

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sp: resolve(__dirname, 'sp/index.html'),
+        brasil: resolve(__dirname, 'brasil/index.html')
+      }
+    }
+  }
+});
